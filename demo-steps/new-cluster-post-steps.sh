5b951fd733a21f5850eaef398a1bc31d838e4122
Albert Wolchesky <albert.wolchesky@outlook.com>
2024-04-02 10:42:01 -0700

    more updates

--- demo-steps/new-cluster-post-steps.sh
+++ demo-steps/new-cluster-post-steps.sh
@@ -1,6 +1,19 @@
+region=east
+
+# Configure Service Mesh
+# https://cloud.google.com/service-mesh/docs/managed/provision-managed-anthos-service-mesh
+gcloud container fleet mesh update \
+    --management automatic \
+    --memberships cluster-$region \
+    --project gke-runrate-reporting \
+    --location us-"$region"1
+
+
 # Apply Group Memberships
 # https://cloud.google.com/kubernetes-engine/docs/how-to/google-groups-rbac
 
-gcloud container clusters update cluster-[REGION] \
-    --region=us-[REGION]1-c \
+gcloud container clusters update cluster-$region \
+    --region=us-"$region"1-c \
     --security-group="gke-security-groups@wolchesky.altostrat.com"
+
+    
